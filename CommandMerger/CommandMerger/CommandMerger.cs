c779e4cde3082731834c0af3430efb6f44e508d8
setpopa <88254510+setpopa@users.noreply.github.com>
2022-10-25 21:43:49 +0200

    Clearer code and config

---
+++ CommandMerger/CommandMerger/CommandMerger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Rocket.Core;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Core.Plugins;
+using Rocket.Core.Logging;
+using CommandMerger.Models;
+
+namespace CommandMerger
+{
+    internal class CommandMerger : RocketPlugin<CommandMergerConfiguration>
+    {
+        public string _permission;
+        public static CommandMergerConfiguration Conf;
+        public static CommandMerger Inst { get; private set; }
+        protected override void Load()
+        {
+            Inst = this;
+            Conf = Configuration.Instance;
+            if (Configuration != null && Configuration.Instance.CommandsMergedsPermissions.Count != 0)
+            {
+                foreach (var commandPermissions in Configuration.Instance.CommandsMergedsPermissions)
+                {
+                    _permission = commandPermissions.Permission;
+                    foreach (var commands in commandPermissions.Commands)
+                    {
+                        CommandCreator cmd = new CommandCreator(_permission,commands.CommandName,commands.Commands);
+                        R.Commands.Register(cmd);
+                    }                  
+                }
+            }
+            Logger.Log($"Plugin {Name} {Assembly.GetName().Version} loaded.");
+        }
+        protected override void Unload()
+        {
+            Inst = null;
+            Conf = null;
+            Logger.Log($"Plugin {Name} unloaded.");
+            foreach (var command in Configuration.Instance.CommandsMergedsPermissions)
+            {
+                R.Commands.DeregisterFromAssembly(this.Assembly);
+            }
+        }
+    }
+}
---
+++ CommandMerger/CommandMerger/CommandMergerConfiguration.cs
@@ -0,0 +1,80 @@
+﻿using Rocket.API;
+using CommandMerger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API.Serialisation;
+using System.Xml.Serialization;
+
+namespace CommandMerger
+{
+
+    public class CommandMergerConfiguration : IRocketPluginConfiguration
+    {
+
+        public HashSet<CommandPermision> CommandsMergedsPermissions;
+        public void LoadDefaults()
+        {
+            CommandsMergedsPermissions = new HashSet<CommandPermision>
+            {
+                new CommandPermision()
+                {
+                    Permission = "commandmerger.newcommand",
+                    Commands = new HashSet<Command>
+                    {
+                        new Command()
+                        {
+                            CommandName = "mycommand",
+                            Commands = new string[]
+                            {
+                                "rocket",
+                                "heal",
+                                "unlimidedammo"
+                            }
+                        }
+                    }
+                },
+                new CommandPermision()
+                {
+                    Permission = "commandmerger.newcommand2",
+                    Commands = new HashSet<Command>
+                    {
+                        new Command()
+                        {
+                            CommandName = "mycommand2",
+                            Commands = new string[]
+                            {
+                                "rocket",
+                                "tp setpopa"
+                            }
+                        }
+                    }
+                },
+            };
+            
+        }
+    }
+    public class CommandPermision
+    {       
+        public string Permission { get; set; }       
+        public HashSet<Command> Commands { get; set; }
+        public CommandPermision()
+        {
+
+        }
+    }
+    public class Command
+    {
+        public string CommandName { get; set; }
+        [XmlArray(ElementName = "Command")]
+        public string[] Commands { get; set; }
+
+        public Command()
+        {
+
+        }
+    }
+
+}
